04405c837611951740522e0bfbf8b4cc09053257
almasami <almasami_3@hotmail.com>
2023-10-15 21:41:26 -0600

    dom

--- comisariado.php
+++ comisariado.php
@@ -25,12 +25,12 @@
   <div class="site-wrap">
     <?php include("./layouts/header.php"); ?>
 <div class="container mt-5">
-        <h1 class="text-center">Comisariado</h1>
+        <h1 class="text-center">Mesa Directiva</h1>
         
-        <table class="table">
+        <table class="styled-table">
             <thead>
                 <tr>
-                    
+                    <th>No.</th>
                     <th>Presidente</th>
                     <th>Secretario</th>
                     <th>Tesorero</th>
@@ -50,10 +50,11 @@
                 while ($fila = $resultado->fetch_assoc()) {
                     echo "<tr>";
                     
-                    echo "<td>" . $fila['Nombre Presidente'] . "</td>";
-                    echo "<td>" . $fila['Nombre Secretario'] . "</td>";
-                    echo "<td>" . $fila['Nombre Tesorero'] . "</td>";
-                    echo "<td>" . $fila['Nombre Tesorero'] . "</td>";
+                    echo "<td>" . $fila['No.'] . "</td>";
+                    echo "<td>" . $fila['Presidente'] . "</td>";
+                    echo "<td>" . $fila['Secretario'] . "</td>";
+                    echo "<td>" . $fila['Tesorero'] . "</td>";
+                    echo "<td>" . $fila['Vigilancia'] . "</td>";
                     echo "<td>" . $fila['Inicio'] . "</td>";
                     echo "<td>" . $fila['Fin'] . "</td>";
                     
@@ -63,6 +64,156 @@
                    
                 }
                 ?>
+               
+  <tr>
+  <th>No.</th>
+    <th>Presidente</th>
+    <th>Secretario</th>
+    <th>Tesorero</th>
+    <th>Vigilancia</th>
+    <th>Inicio</th>
+    <th>Fin</th>
+  </tr>
+  <tr>
+  <th>1</th>
+    <td>Francisco Rojas</td>
+    <td>Abdón Moreno</td>
+    <td>Inocente Mejía</td>
+    <td>Jesús María Chacón</td>
+    <td>1924</td>
+    <td></td>
+  </tr>
+  <tr>
+  <th>2</th>
+    <td>Manuel Luján Varela</td>
+    <td>Mariano López Olivas</td>
+    <td>Ismael Flores Ávila</td>
+    <td>Tomás Lucero Talamantes</td>
+    <td>1971</td>
+    <td></td>
+  </tr>
+  <tr>
+  <th>3</th>
+    <td>Miguel López</td>
+    <td>Rafael Díaz Prado</td>
+    <td>Salvador Baca</td>
+    <td>Victoriano Mora</td>
+    <td>1992</td>
+    <td></td>
+  </tr>
+  <tr>
+  <th>4</th>
+    <td>Manuel Noé Heras Luján</td>
+    <td>Marín Alonso Martínez Chávez</td>
+    <td>José Luis Pérez Meléndez</td>
+    <td>Gilberto Bañuelos González</td>
+    <td>1992</td>
+    <td>1995</td>
+  </tr>
+  <tr>
+  <th>5</th>
+    <td>José María Baca</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td>1996</td>
+    <td>1999</td>
+  </tr>
+  <tr>
+  <th>6</th>
+    <td>Javier Mendoza Valdez</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td>1999</td>
+    <td>2002</td>
+  </tr>
+  <tr>
+  <th>7</th>
+    <td>Manuel Noé Heras Luján</td>
+    <td>Isidro Meléndez Meléndez.</td>
+    <td>Israel Rentería Heras</td>
+    <td>Amadito Olvera Hernández</td>
+    <td>2002</td>
+    <td>2005</td>
+  </tr>
+  <tr>
+  <th>8</th>
+    <td>Héctor Gallegos Esparza</td>
+    <td>Rafael Díaz Prado</td>
+    <td>Julián Ledezma</td>
+    <td>Jesús Veloz Trejo</td>
+    <td>2005</td>
+    <td>2008</td>
+  </tr>
+  <tr>
+    
+  <th>9</th>
+    <td>Mónico Corona Rentería</td>
+    <td>Javier Pérez Meléndez</td>
+    <td>José Luis Pérez Ramírez</td>
+    <td>Delfino Mora Villalba</td>
+    <td>2008</td>
+    <td>2011</td>
+
+  </tr>
+  <tr>
+  <th>10</th>
+    <td>Saúl Baca Carbajal</td>
+    <td>Israel Rentería Heras</td>
+    <td>Armando Rodríguez Guillén</td>
+    <td>Juan Alberto López Escalante</td>
+    <td>2011</td>
+    <td>2014</td>
+  </tr>
+  <tr>
+  <th>11</th>
+    <td>Javier Marín González</td>
+    <td>Rubén Rodríguez Guillén</td>
+    <td>Jorge Corona Guillén</td>
+    <td>Benigno Mora Sánchez</td>
+    <td>2014</td>
+    <td>2017</td>
+  </tr>
+  <tr>
+  <th>12</th>
+    <td>Mauro Corona Quezada</td>
+    <td>José Corona Rentería</td>
+    <td>Samuel López Quezada</td>
+    <td>Felipe Rentería Heras</td>
+    <td>2017</td>
+    <td>2020</td>
+  </tr>
+  <tr>
+  <th>13</th>
+    <td>Rubén Rodríguez Guillén</td>
+    <td>José Fermín Villa Ortiz</td>
+    <td>Miguel Ángel Amaya Mora</td>
+    <td>Rubén López Escalante</td>
+    <td>2020</td>
+    <td>2022</td>
+  </tr>
+  <tr>
+  <th>14</th>
+    <td>Alma Rosa Díaz Torres</td>
+    <td>José Fermín Villa Ortiz</td>
+    <td>Miguel Ángel Amaya Mora</td>
+    <td>Rubén López Escalante</td>
+    <td>2022</td>
+    <td>2023</td>
+
+  </tr>
+  <tr>
+  <th>15</th>
+    <td>Cruz Rentería Heras</td>
+    <td>Eduardo Martínez Rentería</td>
+    <td>Isaí Daniel López Aldavaz</td>
+    <td>Felipe Rentería Heras</td>
+    <td>2023</td>
+    <td></td>
+
+  </tr>
+
             </tbody>
         </table>
     </div> 
