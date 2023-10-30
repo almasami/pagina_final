e7d2740060adbe5da47d762fdfb7e88d5e37c830
almasami <almasami_3@hotmail.com>
2023-10-30 16:48:14 -0600

    pacas

--- php/conexion.php
+++ php/conexion.php
@@ -3,5 +3,6 @@ $servidor='localhost';
 $usuario='root';
 $contra='';
 $bd='e_j_m_o';
-$conexion=mysqli_connect($servidor,$usuario, $contra, $bd) or die ("Ha ocurrido un problema en la conexion");
+$conexion=mysqli_connect($servidor,$usuario, $contra, $bd) 
+or die ("Ha ocurrido un problema en la conexion");
 ?>
